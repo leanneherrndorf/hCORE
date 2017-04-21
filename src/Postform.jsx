209e19a481e7ade6fbfab81f17653464b39fa08d
Brendan Walker <Brendan@Macbooks-MacBook-Pro.local>
2017-04-20 18:24:49 -0700

    Adds button to submit values in input field

--- src/Postform.jsx
+++ src/Postform.jsx
@@ -3,15 +3,25 @@ import React, {Component} from 'react';
 class Postform extends React.Component {
   constructor(props) {
     super(props);
+    this.textInput = null;
   }
+
+  handleClick() {
+    console.log(this.textInput.value);
+    let input = this.textInput.value;
+    this.props.updateMessageOnClick(input);
+    this.textInput.value = null;
+  }
+  
   render() {
     return (
       <footer className="postlist">
-        <input className="postlist" placeholder="What do you have to say?" onKeyPress={this.props.updateMessageOnEnter}/>
-        {/*<button type="button">SUBMIT TO ME</button>*/}
+        <input type='text' ref={(thisInput) => {this.textInput = thisInput;}} className="postlist" placeholder="What do you have to say?" />
+        <input type='submit' value='Submit to me' onClick={this.handleClick.bind(this)} />
       </footer>
     );
   }
 }
+
 
 export default Postform;
