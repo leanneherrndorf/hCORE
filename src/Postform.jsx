45110f680cb21f3c306a9a3aa562681f997cfea4
Brendan Walker <Brendan@Macbooks-MacBook-Pro.local>
2017-04-21 19:24:40 -0700

    Adds alert message if typing more than 200 characters

--- src/Postform.jsx
+++ src/Postform.jsx
@@ -1,25 +1,47 @@
 import React, {Component} from 'react';
 import {Button} from 'react-bootstrap';
+import AlertContainer from 'react-alert';
+
 
 class Postform extends React.Component {
   constructor(props) {
     super(props);
     this.textInput = null;
+    this.alertOptions = {
+      offset: 14,
+      position: 'top right',
+      theme: 'light',
+      time: 5000,
+      transition: 'scale'
+    };
+  }
+
+
+  showAlert(){
+    msg.show('Post must be less than 200 characters.', {
+      time: 2000,
+      type: 'error',
+      icon: <img src="../images/patgar.jpg" style={{width: 60, height: 60, borderRadius: 6 }} />
+    });
   }
 
   handleClick() {
     console.log(this.textInput.value);
     let input = this.textInput.value;
-    this.props.updateMessageOnClick(input);
-    this.textInput.value = null;
+    if (input.length <= 200) {
+      this.props.updateMessageOnClick(input);
+      this.textInput.value = null;
+    } else {
+      this.showAlert();
+    }
   }
 
   render() {
     return (
       <footer className="postlist">
+        <AlertContainer ref={(a) => global.msg = a} {...this.alertOptions} />
         <input type='text' ref={(thisInput) => {this.textInput = thisInput;}} className="postlist" placeholder="What do you have to say?" />
         <Button type='submit' bsStyle="success" onClick={this.handleClick.bind(this)}>Submit to me</Button>
-
       </footer>
     );
   }
