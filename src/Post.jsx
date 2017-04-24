0180162598072d7e4ffe86e5b1384c5aed39d8bf
max-bergen <maxbergen1234@gmail.com>
2017-04-23 17:01:18 -0700

    added praise button

--- src/Post.jsx
+++ src/Post.jsx
@@ -18,7 +18,7 @@ class Post extends React.Component {
 
   render() {
     const percentageLost = this.props.post.health/this.props.post.maxHealth
-    if (this.props.post.health > 0){
+    if (this.props.post.health === this.props.post.maxHealth){
       return (
           <article className="post">
             <span className="health">{this.props.post.health}</span>
@@ -26,11 +26,22 @@ class Post extends React.Component {
               <div style={{width: 300 * percentageLost, backgroundColor: 'tomato', height: 10}}></div>
             </div>
             <p className="post-content">{this.props.post.post}</p>
-            <Button bsStyle="danger" onClick={this.praiseClick.bind(this)}>Praise</Button>
             <Button bsStyle="danger" onClick={this.malaiseClick.bind(this)}>Malaise</Button>
           </article>
       );
       
+    } else if (this.props.post.health !== this.props.post.maxHealth && this.props.post.health > 0) {   
+       return (
+          <article className="post">
+            <span className="health">{this.props.post.health}</span>
+            <div style={{border: '1px solid black', width: 302}}>
+              <div style={{width: 300 * percentageLost, backgroundColor: 'tomato', height: 10}}></div>
+            </div>
+            <p className="post-content">{this.props.post.post}</p>
+            <Button bsStyle="danger" onClick={this.malaiseClick.bind(this)}>Malaise</Button>
+            <Button bsStyle="danger" onClick={this.praiseClick.bind(this)}>Praise</Button>  
+          </article>
+       );
     } else {
       return (
           <article className="post"> 
