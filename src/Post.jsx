0b14300f1ecd3fae4ef59bd085a7cbdd3cf54c94
Brendan Walker <brenny_walker@hotmail.com>
2017-04-24 20:10:36 -0700

    Adds extra styling to app

--- src/Post.jsx
+++ src/Post.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Button} from 'react-bootstrap';
+import {Fade} from 'react-bootstrap';
 
 class Post extends React.Component {
   constructor(props) {
@@ -46,20 +47,19 @@ class Post extends React.Component {
       return (
           <article className="post">
             <span className="health">{this.props.post.health}</span>
-            <div style={{border: '1px solid black', width: 302}}>
-              <div style={{width: 300 * percentageLost, backgroundColor: 'tomato', height: 10}}></div>
+            <div style={{width: 302}}>
+              <div className="health-bar" style={{width: 300 * percentageLost, height: 10, borderRadius: 4}}></div>
             </div>
             <p className="post-content">{this.props.post.post}</p>
             {this.showMalaiseButton()}
           </article>
       );
-
     } else if (this.props.post.health !== this.props.post.maxHealth && this.props.post.health > 0) {
        return (
           <article className="post">
             <span className="health">{this.props.post.health}</span>
-            <div style={{border: '1px solid black', width: 302}}>
-              <div style={{width: 300 * percentageLost, backgroundColor: 'tomato', height: 10}}></div>
+            <div style={{width: 302}}>
+              <div className="health-bar" style={{width: 300 * percentageLost, height: 10, borderRadius: 4}}></div>
             </div>
             <p className="post-content">{this.props.post.post}</p>
             {this.showMalaiseButton()}
@@ -68,10 +68,10 @@ class Post extends React.Component {
        );
     } else {
       return (
-          <article className="post">
-            <img src="../images/gravestone.png" className="grave-stone" style={{width: 120, height: 120}}/>
-            <p className="grave-name">{this.props.post.name}</p>
-          </article>
+        <article className="post">
+          <img src="../images/gravestone.png" className="grave-stone" style={{width: 120, height: 120}}/>
+          <p className="grave-name">{this.props.post.name}</p>
+        </article>
       );
     }
   }
--- src/RoundTimer.jsx
+++ src/RoundTimer.jsx
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class RoundTimer extends Component {
   constructor(props){
     super(props);
-    this.state = {currentCount: 5}
+    this.state = {currentCount: 20}
   }
   timer() {
     this.setState({
